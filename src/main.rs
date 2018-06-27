b5da44dcd0952380442738162f20bfbfc94e347c
twetzel59 <twetzel59@gmail.com>
2018-06-27 14:44:20 -0400

    Third order curves

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ use sfml::window::*;
 
 const WIN_SIZE: (u32, u32) = (800, 600);
 const WIN_TITLE: &str = "polynomial-renderer";
-const POINT_COLOR: Color = Color::CYAN;
-const LEG_COLOR: Color = Color::MAGENTA;
+//const POINT_COLOR: Color = Color::CYAN;
+//const LEG1_COLOR: Color = Color::MAGENTA;
+//const LEG2_COLOR: Color = Color::YELLOW;
 const PATH_COLOR: Color = Color::WHITE;
 
 fn main() {
@@ -17,9 +18,10 @@ fn main() {
 	win.set_vertical_sync_enabled(true);
 	
 	let vertex_array = render_curve(&[
-		Vector2f::new(200., 200.),
-		Vector2f::new(300., 400.),
-		Vector2f::new(500., 500.),
+		Vector2f::new(100., 100.),
+		Vector2f::new(700., 200.),
+		Vector2f::new(150., 350.),
+		Vector2f::new(550., 500.),
 	]);
 	
 	'game: loop {
@@ -38,7 +40,7 @@ fn main() {
 	}
 }
 
-fn render_curve(points: &[Vector2f; 3]) -> VertexArray {
+fn render_curve(points: &[Vector2f; 4]) -> VertexArray {
 	let mut vtx_arr = VertexArray::new(PrimitiveType::LineStrip, 0);
 	
 	for f in 0..101 {
@@ -46,13 +48,22 @@ fn render_curve(points: &[Vector2f; 3]) -> VertexArray {
 		
 		println!("{:?}", factor);
 		
-		let point1 = interpolate(factor, points[1], points[0]);
-        //vtx_arr.append(&vtx_color(point1, LEG_COLOR));
+		let point1a = interpolate(factor, points[1], points[0]);
+        //vtx_arr.append(&vtx_color(point1a, LEG1_COLOR));
 		
-		let point2 = interpolate(factor, points[2], points[1]);
-        //vtx_arr.append(&vtx_color(point2, LEG_COLOR));
+		let point2a = interpolate(factor, points[2], points[1]);
+        //vtx_arr.append(&vtx_color(point2a, LEG1_COLOR));
+        
+        let point3a = interpolate(factor, points[3], points[2]);
+        //vtx_arr.append(&vtx_color(point3a, LEG1_COLOR));
+        
+        let point1b = interpolate(factor, point2a, point1a);
+        //vtx_arr.append(&vtx_color(point1b, LEG2_COLOR));
+        
+        let point2b = interpolate(factor, point3a, point2a);
+        //vtx_arr.append(&vtx_color(point2b, LEG2_COLOR));
 		
-		let curve_point = interpolate(factor, point2, point1);
+		let curve_point = interpolate(factor, point2b, point1b);
 		vtx_arr.append(&vtx(curve_point));
 	}
 	
